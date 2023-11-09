4874029a6beddc6dbd632ecd524a36607e2301a5
Divit <divitajmera15@gmail.com>
2023-11-09 18:06:18 +0530

    initial commit

---
+++ client/src/components/UserListSideNav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function UserListSideNav() {
+  return (
+    <nav className="w-1/5  border-r-2">
+      <ul>user1</ul>
+      <ul>user2</ul>
+    </nav>
+  );
+}
+
+export default UserListSideNav;
